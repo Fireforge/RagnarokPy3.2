bf1ddd34c4737ac50cc2cfd60d361291d8fc82c9
David Salter <fireforge124@gmail.com>
2012-09-11 18:22:39 -0400

    fixed copy portion on publish_docs

--- publish_docs.bat
+++ publish_docs.bat
@@ -1,7 +1,7 @@
 echo "Publishing to Github pages..."
 git checkout gh-pages
 git checkout master sphinx/gh_pages_build/html
-cp .\sphinx\gh_pages_build\html\* . -force -r
+cp sphinx/gh_pages_build/html/* . -f -r
 rm sphinx -r
 git add -A
 git commit -a -m "Generated gh-pages"
