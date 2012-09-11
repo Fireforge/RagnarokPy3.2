cea5bbdc98391b1d87455d25db083042fffd7120
David Salter <fireforge124@gmail.com>
2012-09-11 17:57:00 -0400

    added publish.bat

---
+++ publish_docs.bat
@@ -0,0 +1,12 @@
+echo "Publishing to Github pages..."
+git checkout gh-pages
+git checkout master sphinx/gh_pages_build/html
+cp .\sphinx\gh_pages_build\html\* . -force -r
+rm sphinx -r
+git add -A
+git commit -a -m "Generated gh-pages"
+git push origin gh-pages
+git checkout master
+
+echo "Done!"
+pause
