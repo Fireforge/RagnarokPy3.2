9addbf13391071e924dbe66cff5f45b67880b03b
U-DAS-PC\DAS <DAS@DAS-PC.(none)>
2012-09-11 17:49:04 -0400

    Generated gh-pages

---
+++ generate_docs.bat
@@ -0,0 +1,7 @@
+echo "Generating Sphinx documentation pages..."
+cd sphinx
+./make.bat html
+cd ..
+
+echo "Done!"
+pause
--- publish_docs.bat
+++ publish_docs.bat
@@ -1,7 +1,11 @@
-echo "Building Sphinx docs..."
-cd sphinx
-./make html
-cd ..
-
 echo "Publishing to Github pages..."
 git checkout gh-pages
+git checkout master sphinx/gh_pages_build/html
+cp .\sphinx\gh_pages_build\html\* . -force -r
+rm sphinx -r
+git add -A
+git commit -a -m "Generated gh-pages"
+git push origin gh-pages
+
+echo "Done!"
+pause
